88605a8d67c95e85d345612043f823784806240d
Maju França <juliafkoch@gmail.com>
2022-09-19 04:29:30 -0300

    Projeto Blog Pessoal em React

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Home from './paginas/Home/Home'
+
+let nome = "Majuzinha"
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
---
+++ src/paginas/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className='titulo'>Home do Site</h1>
+            <img className='img' src="https://avatars.githubusercontent.com/u/108549564?v=4" alt="Imagem Tela Inicial" />
+            <h2 className='nome'>Dona: Maju França</h2>
+        </>
+    );
+}
+
+
+export default Home;
